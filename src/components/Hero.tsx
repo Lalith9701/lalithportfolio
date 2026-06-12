88b8ff07fde3717d12947c013c32c33fa7dcf383
lalith charan <lalithcharan111@gmail.com>
2026-06-12 21:38:46 +0530

    Switch to Aurora Night theme (#0D0D1A) with Purple and Cyan accents

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -67,7 +67,7 @@ const Hero = () => {
             <Float speed={2} rotationIntensity={1} floatIntensity={1}>
               <Sphere args={[1, 64, 64]} scale={1.2} position={[2.5, 0.5, -2]}>
                 <MeshDistortMaterial
-                  color="#38BDF8"
+                  color="#A855F7"
                   distort={0.4}
                   speed={2}
                   roughness={0.2}
@@ -76,7 +76,7 @@ const Hero = () => {
               </Sphere>
               <Sphere args={[1, 64, 64]} scale={0.8} position={[-2.5, 1.5, -3]}>
                 <MeshDistortMaterial
-                  color="#7DD3FC"
+                  color="#22D3EE"
                   distort={0.5}
                   speed={1.5}
                   roughness={0.2}
